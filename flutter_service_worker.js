e6438cd76806c619bcc08cdae04337682cbf7f7a
freiSMS <Andre.Heraucourt@web.de>
2020-03-10 22:49:13 +0100

    Update flutter_service_worker.js

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -56,7 +56,6 @@ self.addEventListener('fetch', function (event) {
           return response;
         }
         return fetch(event.request, {
-          credentials: 'include'
         });
       })
   );
